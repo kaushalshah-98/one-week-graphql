3e24edabc49d8593254f3dac910360718f2fd896
Kaushal Shah <kaushal.shah.8398@gmail.com>
2022-06-09 16:04:48 +0530

    install-and-configure-graphql-code-generator

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,12 +1,13 @@
 import { createServer } from "@graphql-yoga/node";
 import { readFileSync } from "node:fs";
 import { join } from "node:path";
+import type { Resolvers } from "../../types";
 
 const typeDefs = readFileSync(join(process.cwd(), "schema.graphql"), {
   encoding: "utf8",
 });
 
-const resolvers = {
+const resolvers: Resolvers = {
   Query: {
     cart: (_, { id }) => {
       return { id, totalItems: 0 };
