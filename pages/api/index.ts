a499065a44db255758cfe473b419f959e9ff0d61
Kaushal Shah <kaushal.shah.8398@gmail.com>
2022-06-09 17:10:01 +0530

    install-and-configure-prisma

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -2,10 +2,23 @@ import { createServer } from "@graphql-yoga/node";
 import { readFileSync } from "node:fs";
 import { join } from "node:path";
 import type { Resolvers } from "../../types";
+import prisma from "../../lib/prisma";
+import type { PrismaClient } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export type GraphQLContext = {
+  prisma: PrismaClient;
+};
 
 const typeDefs = readFileSync(join(process.cwd(), "schema.graphql"), {
   encoding: "utf8",
 });
+
+export async function createContext(): Promise<GraphQLContext> {
+  return {
+    prisma,
+  };
+}
 
 const resolvers: Resolvers = {
   Query: {
@@ -15,12 +28,13 @@ const resolvers: Resolvers = {
   },
 };
 
-const server = createServer({
+const server = createServer<{ req: NextApiRequest; res: NextApiResponse }>({
   endpoint: "/api",
   schema: {
     typeDefs,
     resolvers,
   },
+  context: createContext(),
 });
 
 export default server;
