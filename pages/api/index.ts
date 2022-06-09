8ffe8ab5b024b880e8ee9985a6604294303922b9
Kaushal Shah <kaushal.shah.8398@gmail.com>
2022-06-09 18:13:20 +0530

    create schema, migration and resolver

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -5,10 +5,13 @@ import type { Resolvers } from "../../types";
 import prisma from "../../lib/prisma";
 import type { PrismaClient } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
+import currencyFormatter from "currency-formatter";
 
 export type GraphQLContext = {
   prisma: PrismaClient;
 };
+
+const currencyCode = "USD";
 
 const typeDefs = readFileSync(join(process.cwd(), "schema.graphql"), {
   encoding: "utf8",
@@ -22,8 +25,36 @@ export async function createContext(): Promise<GraphQLContext> {
 
 const resolvers: Resolvers = {
   Query: {
-    cart: (_, { id }) => {
-      return { id, totalItems: 0 };
+    cart: async (_, { id }, { prisma }) => {
+      let cartData = await prisma.cart.findUnique({ where: { id } });
+      if (!cartData) {
+        cartData = await prisma.cart.create({ data: { id } });
+      }
+      return cartData;
+    },
+  },
+  Cart: {
+    items: async ({ id }, _, { prisma }) => {
+      const cartItems = await prisma.cart.findUnique({ where: { id } }).items();
+      return cartItems;
+    },
+    totalItems: async ({ id }, _, { prisma }) => {
+      let cartItems = await prisma.cart.findUnique({ where: { id } }).items();
+      return cartItems.reduce((total, item) => total + item.quantity ?? 1, 0);
+    },
+    subTotal: async ({ id }, _, { prisma }) => {
+      let cartItems = await prisma.cart.findUnique({ where: { id } }).items();
+
+      const amount =
+        cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0) ??
+        0;
+
+      return {
+        formatted: currencyFormatter.format(amount / 100, {
+          code: currencyCode,
+        }),
+        amount,
+      };
     },
   },
 };
